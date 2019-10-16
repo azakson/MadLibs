878b415ba612e990eff8c5d88b48895b018e2d80
Avery <Zakson>
2019-10-16 10:27:03 -0500

    Initial Commit

---
+++ MadLibs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MadLibs
+//
+//  Created by Avery Zakson on 10/16/19.
+//  Copyright © 2019 mojo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
